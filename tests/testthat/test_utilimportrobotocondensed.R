f2998f694bee7b6c960f3f71526f36e67c0bed38
marcosci <sciaini.marco@gmail.com>
2018-03-15 14:36:46 +0100

    Initial commit

---
+++ tests/testthat/test_utilimportrobotocondensed.R
@@ -0,0 +1,10 @@
+# nolint start
+
+context("util_import_roboto_condensed")
+
+test_that("basic functionality", {
+  skip_on_cran()
+  expect_error(util_import_roboto_condensed(), NA)
+})
+
+# nolint end
