f63ac3e8fc93d6c10b1f1eb4bec976689b2e44d7
Unknown <sciaini.marco@gmail.com>
2019-01-23 11:23:44 -0600

    trial round 2

--- tic.R
+++ tic.R
@@ -12,11 +12,11 @@ if (Sys.getenv("id_rsa") != "") {
     if (ci()$get_branch() == "master") {
         get_stage("deploy") %>%
             add_code_step(rmarkdown::render("README.Rmd", "github_document")) %>%
-            add_step(step_push_deploy(path = "README.md", "master")) %>%
-            add_code_step(devtools::document()) %>%
-            add_step(step_push_deploy(path = c("man/", "DESCRIPTION", "NAMESPACE", "master"))) %>%
-            add_step(step_build_pkgdown()) %>%
-            add_step(step_push_deploy("docs", "gh-pages"))
+            add_step(step_push_deploy(path = "README.md", "master")) # %>%
+            # add_code_step(devtools::document()) %>%
+            # add_step(step_push_deploy(path = c("man/", "DESCRIPTION", "NAMESPACE", "master"))) %>%
+            # add_step(step_build_pkgdown()) %>%
+            # add_step(step_push_deploy("docs", "gh-pages"))
     }
 
     get_stage("after_success") %>%
