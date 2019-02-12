f3fa809bc8726c62062c0adcb366bbbdb212e42e
marco <sciaini.marco@gmail.com>
2019-02-12 15:03:01 +0100

    fix roxygen on travis

--- tic.R
+++ tic.R
@@ -15,7 +15,7 @@ if (Sys.getenv("id_rsa") != "") {
             add_step(step_push_deploy(commit_paths = "README.md", branch = "master")) %>%
             add_code_step(devtools::document()) %>%
             add_step(step_push_deploy(commit_paths = c("man/", "DESCRIPTION", "NAMESPACE", branch =  "master"))) %>%
-            add_code_step(pkgbuild::compile_dll()) %>%
+            add_code_step(remotes::install_version("roxygen2", "6.1.0")) %>%
             add_step(step_build_pkgdown()) %>%
             add_step(step_push_deploy("docs", "gh-pages"))
     }
