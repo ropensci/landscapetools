4b324b2433deb7b616e24f1a630718421827c16e
Michael Sumner <mdsumner@gmail.com>
2025-12-02 23:19:49 +1100

    Update ggplot2 4.0.0 (#47)
    * fix for ggplot2 4.0.0 (family = NULL, not NA)
    
    * recreate inbuilt raster data
    
    * use inherits not class ==
    
    * no aes_string
    
    * recreate datasets properly
    
    * rlang and ggplot aes
    
    * inherits not class ==
    
    * minor vignette
    
    * readme
    
    * REALLY fix the raster srs issue

--- data-raw/examplemaps.R
+++ data-raw/examplemaps.R
@@ -1,15 +1,43 @@
 # Example maps from NLMR -------
 
-fractal_landscape <- NLMR::nlm_fbm(ncol = 150, nrow = 150, fract_dim = 0.8)
+# fractal_landscape <- NLMR::nlm_fbm(ncol = 150, nrow = 150, fract_dim = 0.8)
+# usethis::use_data(fractal_landscape, overwrite = TRUE)
+#
+# gradient_landscape <- NLMR::nlm_planargradient(150, 150)
+# usethis::use_data(gradient_landscape, overwrite = TRUE)
+#
+# random_landscape <- NLMR::nlm_random(150, 150)
+# usethis::use_data(random_landscape, overwrite = TRUE)
+#
+# # Classify the map into land uses
+# classified_landscape <- util_classify(fractal_landscape,
+#                         n = 3,
+#                         level_names = c("Land Use 1",
+#                                         "Land Use 2",
+#                                         "Land Use 3"))
+#
+#
+# usethis::use_data(classified_landscape, overwrite = TRUE)
+
+#remotes::install_github("ropensci/landscapetools")
+
+library(landscapetools)
+# fractal_landscape <- NLMR::nlm_fbm(ncol = 150, nrow = 150, fract_dim = 0.8)
+x <- fractal_landscape
+fractal_landscape <- raster::setValues(raster::raster(x), raster::values(x))
 usethis::use_data(fractal_landscape, overwrite = TRUE)
-
-gradient_landscape <- NLMR::nlm_planargradient(150, 150)
+#
+# gradient_landscape <- NLMR::nlm_planargradient(150, 150)
+x <- gradient_landscape
+gradient_landscape <- raster::setValues(raster::raster(x), raster::values(x))
 usethis::use_data(gradient_landscape, overwrite = TRUE)
-
-random_landscape <- NLMR::nlm_random(150, 150)
+#
+# random_landscape <- NLMR::nlm_random(150, 150)
+x <- random_landscape
+random_landscape <- raster::setValues(raster::raster(x), raster::values(x))
 usethis::use_data(random_landscape, overwrite = TRUE)
-
-# Classify the map into land uses
+#
+# # Classify the map into land uses
 classified_landscape <- util_classify(fractal_landscape,
                         n = 3,
                         level_names = c("Land Use 1",
@@ -18,3 +46,4 @@ classified_landscape <- util_classify(fractal_landscape,
 
 
 usethis::use_data(classified_landscape, overwrite = TRUE)
+
